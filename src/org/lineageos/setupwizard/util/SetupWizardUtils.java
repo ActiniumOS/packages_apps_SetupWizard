crdroidandroid/android_packages_apps_SetupWizard

SetupWizard: Use correct updater package
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: neobuddy89 <neobuddy89@gmail.com>

src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -71,7 +71,7 @@ public class SetupWizardUtils {
     private static final String GMS_PACKAGE = "com.google.android.gms";
     private static final String GMS_SUW_PACKAGE = "com.google.android.setupwizard";
     private static final String GMS_TV_SUW_PACKAGE = "com.google.android.tungsten.setupwraith";
-    private static final String UPDATER_PACKAGE = "org.lineageos.updater";
+    private static final String UPDATER_PACKAGE = "com.crdroid.updater";
 
     private static final String UPDATE_RECOVERY_EXEC = "/vendor/bin/install-recovery.sh";
     private static final String CONFIG_HIDE_RECOVERY_UPDATE = "config_hideRecoveryUpdate";

ActiniumOS/packages_apps_SetupWizard

Change-Id: I145eabf99ec1e0b6f46778810b38acbde6b39b56
Signed-off-by: AnierinB <anierin@evolution-x.org>

src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -71,7 +71,7 @@ public class SetupWizardUtils {
     private static final String GMS_PACKAGE = "com.google.android.gms";
     private static final String GMS_SUW_PACKAGE = "com.google.android.setupwizard";
     private static final String GMS_TV_SUW_PACKAGE = "com.google.android.tungsten.setupwraith";
-    private static final String UPDATER_PACKAGE = "org.lineageos.updater";
+    private static final String UPDATER_PACKAGE = "org.evolution.updater";
 
     private static final String UPDATE_RECOVERY_EXEC = "/vendor/bin/install-recovery.sh";
     private static final String CONFIG_HIDE_RECOVERY_UPDATE = "config_hideRecoveryUpdate";